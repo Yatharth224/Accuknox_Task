b53941a8634aca8612026bd478d730d2f612e9b0
Your Name <yatharthchouhan22@gmail.com>
2026-06-15 08:08:00 +0530

    spacing correction

--- signals_task/signals.py
+++ signals_task/signals.py
@@ -9,32 +9,31 @@ from django.db import connection
 # Q1:
 # Are django signals executed synchronously or asynchronously?
 
-
 @receiver(post_save, sender=Demo_model)
 def first_question(sender, instance, **kwargs):
     print("Signal started")
-    time.sleep(7)  # Hence it proves that django signals works synchronously . 
+    time.sleep(7)  #  The 7-second delay blocks the caller until the signal execution is completed
+                   #Hence it proves that django signals works synchronously . 
+   
     print("Signal finished")
-
 
 
 # Q2:
 # Do django signals run in the same thread as the caller?
 
-
 @receiver(post_save, sender=Demo_model)
 def second_question(sender, instance, **kwargs):
-    print(threading.current_thread().name)      # here signal and caller print same thread name then it show it runs in same thread 
+    print(threading.current_thread().name)      # here signal and caller print same thread name 
+                                                 # then it show it runs in same thread 
 
 
-
-
-    
 # Q3:
 # Do django signals run in the same database transaction as the caller?
 @receiver(post_save, sender=Demo_model)
 def third_question(sender, instance, **kwargs):
     if connection.in_atomic_block:
-        print("Inside transaction") #  If this prints "Inside transaction" while the caller is inside a transaction, it indicates that the signal is running in the same database transaction as the caller.
+        print("Inside transaction") #  If this prints "Inside transaction" while the caller is inside a transaction, 
+                                    # it indicates that the signal is running in the same database transaction as the caller.
+   
     else:
         print("Not inside transaction")
